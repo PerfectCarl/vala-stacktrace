2be525b82aa12e79e8d111b64e30197f9b47a5b7
cran <name.is.carl@gmail.com>
2015-01-10 12:26:33 +0100

    First stab at dependencies

---
+++ cmake/Dependencies.cmake
@@ -0,0 +1,16 @@
+# Bundle version: 0.9
+#
+# File History:
+#    - 0.1 : initial release
+
+macro (read_dependency_file)
+
+endmacro()
+
+macro (install_dependencies)
+
+endmacro()
+
+macro (get_vala_package pc_package vala_package)
+
+endmacro()
